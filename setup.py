5901a68fb9c0736217ea411a45dd65dab6e7a935
ankush060 <ankush.22scse1480003@galgotiasuniversity.edu.in>
2025-07-21 03:19:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
     packages= find_packages(),
     install_requires = []
 
